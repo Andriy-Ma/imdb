f35079b3011c5224ebfb3642c63d5e7827dda2e6
Andrii <www.andriy12i@gmail.com>
2023-04-26 18:04:41 +0300

    added search

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Header from '@/components/Header'
 import './globals.css'
 import Providers from './Providers'
 import Navbar from '@/components/Navbar'
+import SearchBox from '@/components/SearchBox'
 
 export const metadata = {
   title: 'IMDB CLONE',
@@ -15,7 +16,7 @@ export default function RootLayout({ children }) {
         <Providers>
           <Header />
           <Navbar />
-          {/* SearchBox */}
+          <SearchBox />
           {children}
         </Providers>
         </body>
